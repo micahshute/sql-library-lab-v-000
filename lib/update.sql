353f21bb5283977ecf7383920cccfeac9c0eaa3c
Micah Shute <micah.shute@gmail.com>
2018-07-30 04:44:43 +0000

    automatically backed up by learn

--- lib/update.sql
+++ lib/update.sql
@@ -1,1 +1,1 @@
-UPDATE characters SET name = "Martian" WHERE characters.id = (SELECT id FROM characters ORDER BY id DESC LIMIT 1);
+UPDATE characters SET species = "Martian" WHERE characters.id = (SELECT id FROM characters ORDER BY id DESC LIMIT 1);
