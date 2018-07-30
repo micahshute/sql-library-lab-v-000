609a8824068cea76b0013cfedb2c9452f31ab9aa
Micah Shute <micah.shute@gmail.com>
2018-07-30 05:01:33 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.name FROM series INNER JOIN books ON series.book_id=books.id JOIN character_books ON books.id=character_books.book_id JOIN characters ON characters.id=character_books.character_id GROUP BY character.species ORDER BY COUNT(*) DESC LIMIT 1"
+  "SELECT series.title FROM series INNER JOIN books ON series.book_id=books.id JOIN character_books ON books.id=character_books.book_id JOIN characters ON characters.id=character_books.character_id GROUP BY character.species ORDER BY COUNT(*) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
