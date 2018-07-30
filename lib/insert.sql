37988bc2ab531f84df9dc42e21e2786e809f85ca
Micah Shute <micah.shute@gmail.com>
2018-07-30 04:39:02 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Chronicles of Narnia", 1, 1), (2, "The Lord of the Rings", 1, 2);
-INSERT INTO subgenres (id, name) VALUES (1, "Fantasy"), ("Epic Style Legend", 2);
+INSERT INTO subgenres (id, name) VALUES (1, "Fantasy"), (2, "Epic Style Legend");
 INSERT INTO authors (id, name) VALUES (1, "C.S. Lewis"), (2, "J.R.R. Tolkien");
 INSERT INTO books (id, title, year, series_id) VALUES (1, "The Lion, the Witch, and the Wardrobe", 1950, 1), (2, "Prince Caspian", 1951, 1), (3, "Voyage of the Dawn Treader", 1952, 1);
 INSERT INTO books (id, title, year, series_id) VALUES (4, "The Fellowship of the Ring", 1954, 2), (5, "The Two Towers", 1954, 2), (6, "The Return of the King", 1955, 2);
